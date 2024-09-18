3c46187d1d2ba2295e206abf0956ab9f0a545e08
erikoborie <eriko.yamazaki10@gmail.com>
2024-09-18 15:45:49 +0800

    Uploaded 2 files

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+
+
+  btn.addEventListener('click', () => {
+    setTimeout(() => {
+      document.getElementById("text").textContent = "ボタンをクリックしました";
+    }, 2000);
+  });
+
