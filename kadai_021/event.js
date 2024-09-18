290ada092e9a4e34464ad72764e667ecac7eb02a
erikoborie <eriko.yamazaki10@gmail.com>
2024-09-18 16:05:56 +0800

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-
-
-  btn.addEventListener('click', () => {
-    setTimeout(() => {
-      document.getElementById("text").textContent = "ボタンをクリックしました";
-    }, 2000);
-  });
-
